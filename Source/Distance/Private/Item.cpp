617533cdc2f1be46899dae14a231d5bcd36c2d17
Brittany West <bkwest@ucsc.edu>
2015-02-08 16:23:57 -0800

    I'm merging?

--- Source/Distance/Private/Item.cpp
+++ Source/Distance/Private/Item.cpp
@@ -10,16 +10,30 @@ AItem::AItem(const FObjectInitializer& ObjectInitializer)
 	amount = 100.0f;
 	maxAmount = 100.0f;
 
+
+	RootSceneComponent = ObjectInitializer.CreateDefaultSubobject<USceneComponent>(this, TEXT("RootSceneComponent"));
+	RootComponent = RootSceneComponent;
+
 	SpriteComponent = ObjectInitializer.CreateDefaultSubobject<UPaperSpriteComponent>(this, TEXT("SpriteComponent"));
 	SpriteComponent->AttachTo(RootComponent);
 	SpriteComponent->RelativeRotation = FRotator(0.f, 90.f, -60.f);
-
-	
+	SpriteComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	//SpriteComponent->SetSprite(ConstructorHelpers::FClassFinder<UPaperSprite> (TEXT("/Game/Sprites/Lantern_Sprite")));
 }
 
 void AItem::Use()
 {
-
+	bIsEnabled = !bIsEnabled;
+	if (bIsEnabled)//may or may not need to check this if im just toggling
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Item is: Enabled"));
+		SpriteComponent->SetVisibility(true);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Item is: Disabled"));
+		SpriteComponent->SetVisibility(false);
+	}
 }
 
 void AItem::OnEquip()

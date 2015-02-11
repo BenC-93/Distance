b15b90086ebac9a70a03de149c0acc632feeda09
Jordan Sommers <jtsommer@ucsc.edu>
2015-02-10 21:18:45 -0800

    Add printScreen method for putting quick debug messages on screen

--- Source/Distance/Public/Distance.h
+++ Source/Distance/Public/Distance.h
@@ -4,11 +4,16 @@
 #define __DISTANCE_H__
 
 #include "EngineMinimal.h"
+// We probably shouldn't be importing the entire engine here, but fast debugging.
+// TODO: remove for real builds
+#include "Engine.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(LogDistance, Log, All);
 
 const float DEFAULT_SPRITE_PITCH = 0.0f;
 const float DEFAULT_SPRITE_YAW = 90.0f;
 const float DEFAULT_SPRITE_ROLL = -65.0f;
+
+#define printScreen(c, text) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 5.0, c, text);
 
 #endif

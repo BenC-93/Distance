2330ddc1ec870f555450f66a23e123b68ad3df1d
Ben Cooney <bcooney@ucsc.edu>
2015-05-15 21:47:50 -0700

    Started to flesh out AIBoss_Betrayal_Minion class.

--- Source/Distance/Public/AIBoss_Betrayal_Minion.h
+++ Source/Distance/Public/AIBoss_Betrayal_Minion.h
@@ -5,6 +5,8 @@
 #include "GameFramework/Character.h"
 #include "AIBoss_Betrayal_Minion.generated.h"
 
+enum MoveState { STATIC, COPY, FOLLOW, RANDOM };
+
 /**
  * 
  */
@@ -13,7 +15,38 @@ class DISTANCE_API AAIBoss_Betrayal_Minion : public ACharacter
 {
 	GENERATED_BODY()
 	
+public:
+	AAIBoss_Betrayal_Minion(const FObjectInitializer& ObjectInitializer);
 	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Sprite")
+	class UPaperSpriteComponent* SpriteComponent;
+
+	//~~~ Health ~~~//
+	float Health;
+	float MaxHealth;
+
+	void ChangeHealth(float h);
+	float GetHealth();
+	float GetMaxHealth();
 	
-	
+	//~~~ Targeting  ~~~//
+	AActor* TargetActor;
+	FVector TargetLocation;
+	float TargetSpeed;
+
+	void SetTargetActor(AActor* t);
+	AActor* GetTargetActor();
+
+	//~~~ Move states(States) and functions ~~~//
+	MoveState ActiveMoveState;
+
+	void SetMoveState(MoveState m);
+	MoveState GetMoveState();
+
+	void MoveStatic();
+	void MoveCopy();
+	void MoveFollow();
+	void MoveRandom();
+
+	void StartMoveRandomTimer();
 };

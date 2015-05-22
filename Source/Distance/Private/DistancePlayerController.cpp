e86a689ef342333e9d8acdbec4b2202b2e924623
Dylan Markowitz <deeveret@ucsc.edu>
2015-05-22 14:57:21 -0700

    got rid of bindings for playtest

--- Source/Distance/Private/DistancePlayerController.cpp
+++ Source/Distance/Private/DistancePlayerController.cpp
@@ -65,11 +65,11 @@ void ADistancePlayerController::SetupInputComponent()
 	InputComponent->BindAxis("MoveRight", this, &ADistancePlayerController::MoveRight);
 	InputComponent->BindAction("CycleInventory", IE_Pressed, this, &ADistancePlayerController::CycleInventory);
 	InputComponent->BindAction("ItemPickup", IE_Pressed, this, &ADistancePlayerController::ItemPickup);
-	InputComponent->BindAction("ItemDrop", IE_Pressed, this, &ADistancePlayerController::ItemDrop);
+	//InputComponent->BindAction("ItemDrop", IE_Pressed, this, &ADistancePlayerController::ItemDrop);
 
 	//Temp input for crystal
-	InputComponent->BindAction("OtherUseItem", IE_Pressed, this, &ADistancePlayerController::OnOtherUseItemPressed);
-	InputComponent->BindAction("OtherUseItem", IE_Released, this, &ADistancePlayerController::OnUseItemReleased);
+	//InputComponent->BindAction("OtherUseItem", IE_Pressed, this, &ADistancePlayerController::OnOtherUseItemPressed);
+	//InputComponent->BindAction("OtherUseItem", IE_Released, this, &ADistancePlayerController::OnUseItemReleased);
 }
 
 void ADistancePlayerController::MoveForward(float val)
